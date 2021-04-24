45a7fd82b47d066fa679f59972d7cc3bbbcc2261
Pal <58456171+palgramming@users.noreply.github.com>
2021-04-24 13:20:07 -0400

    Add files via upload

---
+++ pricing_component_with_toggle/app.js
@@ -0,0 +1,11 @@
+const timeLength = document.getElementById("switch");
+const price = document.querySelectorAll(".price > span")
+
+timeLength.addEventListener("click", () => {
+   console.log(price)
+   timeLength.classList.toggle("switch-on")
+   timeLength.classList.toggle("switch-off")
+   price.forEach( (p) => p.classList.toggle("nine"))
+})
+
+
