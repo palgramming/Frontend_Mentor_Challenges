3b226d26780db9f5bab5c11aa6fb994922882acc
Pal <58456171+palgramming@users.noreply.github.com>
2021-05-10 19:47:04 -0400

    Add files via upload

---
+++ ping_coming_soon_page/app.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("form");
+const email = document.getElementById('email');
+const error = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (email.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+        email.style.border = "";
+        error.classList.add("none");
+           alert(`Congrats had this been a real "Email Submission" you would have submitted a valid email address "${email.value}"`);
+           email.value = "";
+    } else {
+         email.style.border = "1px solid #FF5466";
+         error.classList.remove("none");
+         email.value = "";
+         email.placeholder = "example@email/com";
+    };
+   
+    
+});
