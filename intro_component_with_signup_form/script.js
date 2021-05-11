bb5a63c49dc70da9b84ad7ca0f6076e651294e07
Pal <58456171+palgramming@users.noreply.github.com>
2021-05-11 17:39:07 -0400

    Add files via upload

---
+++ intro_component_with_signup_form/script.js
@@ -0,0 +1,71 @@
+const firstError = document.getElementById("first-error")
+const lastError = document.getElementById("last-error")
+const emailError = document.getElementById("email-error")
+const passwordError = document.getElementById("password-error")
+const form = document.getElementById("form")
+const email = document.getElementById("email")
+const inputs = Array.from(document.getElementsByTagName("input"))
+
+const firstTE = document.getElementById("first-text-error");
+const lastTE = document.getElementById("last-text-error");
+const emailTE = document.getElementById("email-text-error");
+const passwordTE = document.getElementById("password-text-error");
+
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    let errorCount = 0;
+    if (inputs[0].value.length < 1) {
+         firstTE.classList.remove("error");
+    firstError.classList.remove("error");
+    errorCount +=1
+    } else {
+firstTE.classList.add("error");
+    firstError.classList.add("error");
+    }
+   if (inputs[1].value.length < 1) {
+        lastTE.classList.remove("error");
+         lastError.classList.remove("error");
+         errorCount +=1
+    } else {
+            lastTE.classList.add("error");
+         lastError.classList.add("error");
+    }
+    if (!inputs[2].value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        emailTE.classList.remove("error");
+        emailError.classList.remove("error");
+        errorCount +=1
+    } else {
+        emailTE.classList.add("error");
+        emailError.classList.add("error");
+    }
+   
+     if (inputs[3].value.length < 1) {
+        passwordTE.classList.remove("error");
+        passwordError.classList.remove("error");
+        errorCount +=1
+    } else {
+        passwordTE.classList.add("error");
+        passwordError.classList.add("error")
+    }
+   
+
+  
+    if (errorCount == 0) {
+        alert(`Congrats your submission was accepted
+        name: ${inputs[0].value} ${inputs[1].value}
+        email: ${inputs[2].value}
+        password: ***hidden***`)
+
+        inputs.forEach((e) => e.value = "" )
+        
+    }
+    
+   
+    
+})
+
+
+
+
+
